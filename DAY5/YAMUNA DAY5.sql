a8633e27a780491ccaf22d791431a2b7d5ec046a
YamunaManoj <yamuna.kbr@gmail.com>
2025-04-29 13:53:12 -0500

    Add files via upload

---
+++ DAY5/YAMUNA DAY5.sql
@@ -0,0 +1,97 @@
+
+/******************************* YAMUNA DAY5 Assignment ****************************/
+
+
+
+----------/* 1. GROUP BY with WHERE - Orders by Year and Quarter.  
+
+Display order year, quarter, order count, avg freight cost 
+only for those orders where freight cost > 100 */-----------------
+
+
+
+select 
+	extract(year from order_date) as order_year, 
+	extract(quarter from order_date) as order_quarter,
+	count(*) as order_count,
+	round(cast(avg(freight) as numeric), 2) as average_freight_cost
+from 
+	orders
+where 
+	freight > 100
+group by 
+	order_year,
+	order_quarter
+order by 
+	order_year,
+	order_quarter;
+
+SELECT  * FROM orders
+SELECT * FROM order_details
+
+
+/**********  2. GROUP BY with HAVING - High Volume Ship Regions.
+Display ship region, no of orders in each region, 
+min and max freight cost. Filter regions where no of orders >=5   ************/
+
+
+
+select ship_region, count(*) as num_orders, min(freight), max(freight)
+from orders
+group by ship_region
+having count(*) >= 5
+order by num_orders desc;
+
+
+
+
+/* 3. Get all title desginations across employees and customers. (UNION & UNION ALL)*/
+
+
+select title from employees 
+UNION
+select contact_title from customers;
+
+select title from employees 
+UNION ALL
+select contact_title from customers;
+
+
+
+select * from customers;
+select * from employees;
+
+
+/*********** 4. Find categories that have both discontinued and in-stock products. 
+(Display category_id, instock means units_in_stock > 0, Intersect)**************/
+
+
+select category_id, units_in_stock as instock
+from products
+where units_in_stock >0 
+INTERSECT
+select category_id, units_in_stock as instock
+from products
+where discontinued = 0 
+order by instock;
+
+
+select * from categories;
+
+select * from products;
+
+
+/* 5. Find orders that have no discounted items (Display the  order_id, EXCEPT)*/
+
+
+select order_id 
+from orders 
+Except
+select distinct order_id
+from order_details
+where discount = 1;
+
+
+select * from orders;
+
+select * from order_details;
